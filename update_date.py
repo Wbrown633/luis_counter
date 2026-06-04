5aee7bbc2fefc2d02f2dbaa122631feab2f446ae
Will Brown <williamlandisbrown@gmail.com>
2026-06-03 22:48:46 -0400

    add record count

---
+++ update_date.py
@@ -0,0 +1,5 @@
+# import the json values 
+
+# check how far today is from the stored date (this is the value of the counter on the site)
+# check if that is larger than the record, if it is update the record
+# update the date to be either today or the given value 
